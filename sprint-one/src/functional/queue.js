536512e9ed33c911198f764fc9c08a0e61d5c532
xelad1 <xelad1@gmail.com>
2014-10-29 16:14:35 -0700

    Updated to specifics

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+
+    someInstance[counter] = value;
+    counter += 1;
   };
 
   someInstance.dequeue = function(){
+
+    var results = [];
+    results.push(someInstance[counter - 1]);
+    delete someInstance[counter - 1];
+    counter -= 1;
+
+    return results;
   };
 
   someInstance.size = function(){
+
+    return Math.max(counter, 0);
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,37 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter =  0;
 
   // Implement the methods below
   someInstance.push = function(value){
+
+    someInstance[counter] = value;
+
+    counter ++;
+
   };
 
   someInstance.pop = function(){
+
+
+  var returnValue = [];
+  returnValue.push(someInstance[counter - 1]);
+
+  delete someInstance[counter - 1];
+
+
+  counter --;
+
+  return returnValue[0];
+
   };
 
   someInstance.size = function(){
-  };
+  if(counter<0){return 0;
+  }
+  return counter;
+};
 
   return someInstance;
 };

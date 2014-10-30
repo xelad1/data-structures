709b3c2b534a82bbf9faf8c1cde257370e155e2f
xelad1 <xelad1@gmail.com>
2014-10-30 11:07:30 -0700

    Up to shared Functional

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,37 @@
 var makeQueue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.startValue = 0;
+  newQueue.endValue = 0;
+  newQueue.storage = {};
+  extend(newQueue, queueMethods);
+  return newQueue;
 };
 
 var queueMethods = {};
 
+var extend = function(to, from){
+  for(var key in from){
+    to[key]=from[key];
+  }
+};
 
+queueMethods.enqueue = function(value){
+  this.storage[this.endValue]= value;
+  this.endValue ++;
+};
 
+queueMethods.dequeue = function(){
+  if (this.startValue<this.endValue) {
+  // debugger;
+  var popOff = this.storage[this.startValue];
+  console.log(popOff);
+  delete this.storage[this.startValue];
+  this.startValue ++;
+  return popOff;}
+
+};
+
+queueMethods.size = function(){
+  // return 0;
+  return Math.max(this.endValue - this.startValue,0);
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,58 +1,58 @@
-var makeQueue = function(){
-  var someInstance = {};
+// var makeQueue = function(){
+//   var someInstance = {};
 
-  // Use an object with numeric keys to store values
-  var storage = {};
-  var counter = 0;
-
-
-
-  // Implement the methods below
-
-  someInstance.enqueue = function(value){
-
-    storage[counter] = value;
-    counter += 1;
-
-  };
-
-  someInstance.dequeue = function(){
-
-    var results = [];
-    var keyArray = Object.keys(storage).sort(function(a, b) {return a - b;});
-    var firstKey = keyArray[0];
-    results.push(storage[firstKey]);
-    delete storage[firstKey];
-    return results[0];
-  };
-
-  someInstance.size = function(){
-
-    return Object.keys(storage).length;
-  };
-
-  return someInstance;
-};
-
-
-// var makeQueue = function() {
+//   // Use an object with numeric keys to store values
 //   var storage = {};
-//   var queueStart = 0;
-//   var queueEnd = 0;
-//   var Queue = {};
-//   Queue.enqueue = function(value){
-//     storage[queueEnd] = value;
-//     queueEnd++;
-//   }
-//   Queue.dequeue = function() {
-//     if (! storage[queueStart]) { console.log('EMPTY LINE'); return}
-//     var luckyturn = storage[queueStart];
-//     delete storage[queueStart];
-//     queueStart++;
-//     return luckyturn;
-//   }
-//   Queue.size = function() {
-//     return queueEnd - queueStart;
-//   }
-//   return Queue;
-// }
+//   var counter = 0;
+
+
+
+//   // Implement the methods below
+
+//   someInstance.enqueue = function(value){
+
+//     storage[counter] = value;
+//     counter += 1;
+
+//   };
+
+//   someInstance.dequeue = function(){
+
+//     var results = [];
+//     var keyArray = Object.keys(storage).sort(function(a, b) {return a - b;});
+//     var firstKey = keyArray[0];
+//     results.push(storage[firstKey]);
+//     delete storage[firstKey];
+//     return results[0];
+//   };
+
+//   someInstance.size = function(){
+
+//     return Object.keys(storage).length;
+//   };
+
+//   return someInstance;
+// };
+
+
+var makeQueue = function() {
+  var storage = {};
+  var queueStart = 0;
+  var queueEnd = 0;
+  var Queue = {};
+  Queue.enqueue = function(value){
+    storage[queueEnd] = value;
+    queueEnd++;
+  }
+  Queue.dequeue = function() {
+    if (! storage[queueStart]) { console.log('EMPTY LINE'); return}
+    var luckyturn = storage[queueStart];
+    delete storage[queueStart];
+    queueStart++;
+    return luckyturn;
+  }
+  Queue.size = function() {
+    return queueEnd - queueStart;
+  }
+  return Queue;
+}

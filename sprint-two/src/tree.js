6940bb878bec08bd028bcd2f540a15b634f2084b
xelad1 <xelad1@gmail.com>
2014-10-31 14:26:22 -0700

    Updated through trees!

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,3 +1,6 @@
+
+
+
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
@@ -9,22 +12,55 @@ var makeTree = function(value){
 
 
 var extend = function(to, from) {
-
   for(var key in from) {
     to[key] = from[key];
   }
-
 }
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  this.children = this.children || [];
+  this.children.push(makeTree(value));
+
 
 };
 
 treeMethods.contains = function(target){
+  //console.log(this.children);
+  //console.log((this.children[0]).value +'index');
+  if((this.children[0]).value === target){
+    return true;
+  }
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(this.children);
+  // if(5===target){
+  //   return true;}
+
+  //var startTree = ??
+  //for(var i=0; i<startTree.length; i++){
+  //   if(startTree[i]==target){
+  //     return true;
+  //   } else if(startTree.children){
+  //     (startTree.children).contains(target);
+  //   } else {
+   //    return false;}
 
 
 /*

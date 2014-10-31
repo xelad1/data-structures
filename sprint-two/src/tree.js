244199fc6dde1af22bbf7266421568670ad4f3e6
xelad1 <xelad1@gmail.com>
2014-10-31 11:25:20 -0700

    Passed tree spec 1

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,11 +2,19 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  extend(newTree, treeMethods);
   return newTree;
 };
 
 
 
+var extend = function(to, from) {
+
+  for(var key in from) {
+    to[key] = from[key];
+  }
+
+}
 
 var treeMethods = {};
 
